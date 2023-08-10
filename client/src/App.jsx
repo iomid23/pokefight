f8fc1b8e1c03b32157f773117863eae439323028
Gabriel3004 <gabriel.i.gomez.p@gmail.com>
2023-08-11 00:17:04 +0200

    My Pokedex

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
+import Pokedex from "./component/Pokedex";
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
+    <Pokedex />
      
     </>
   )
