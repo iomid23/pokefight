8708cf2d04aded40f5a8b86befad673d7e0f8990
Gabriel3004 <gabriel.i.gomez.p@gmail.com>
2023-08-17 02:34:14 +0200

    info und style zum Popup

--- client/src/component/PopupGfg.jsx
+++ client/src/component/PopupGfg.jsx
@@ -51,23 +51,59 @@ export default function PopupGfg({ selectedPokemon }) {
               
               <div id="Das ist der info"
               className="flex justify-center flex-col">
+                <div className="flex space-x-4 justify-center">
 
-              <h2 className="font-bold text-white text-2xl flex justify-center pb-8">Type:</h2>
+              <h2 className="font-bold text-white text-2xl flex justify-center ">Type:</h2>
                   <div className="flex justify-center space-x-2 mt-2">
                     {selectedPokemon.types.map((type) => (
                         <span key={type.slot}
-                        className={`px-2 py-1 rounded-lg text-white ${getTypeBackgroundColorClass(
+                        className={`px-2 py-1 rounded-lg text-white 
+                        hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300
+                         ${getTypeBackgroundColorClass(
                           type.type.name )}`}>
                       {type.type.name}
                       </span>
                   ))}
                   </div>
+                  </div>
+
+                  <div id="Aqui" className="flex justify-center flex-col">
+
+                        <h2 className="font-bold text-white text-2xl flex justify-center  pt-4">Abilities:</h2>
+                        <div className="flex justify-center space-x-2 mt-2">
+                            {selectedPokemon.abilities.map((ability) => (
+                                <span key={ability.slot} className="px-2 py-1 rounded-lg text-white bg-blue-500
+                                hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">
+                                    {ability.ability.name}
+                                </span>
+                            ))}
+                        </div>
+
+                        <h2 className="font-bold text-white text-2xl flex justify-center pb-4 pt-4">Stats:</h2>
+                        <div className="flex flex-col">
+                          
+                              {selectedPokemon.stats.map((stat) => (
+                                
+                                  <div 
+                                    key={stat.stat.name} 
+                                    className="px-2 py-1 rounded-lg text-white bg-green-700 flex justify-between mb-2 
+                                     hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">
+                                      <div>
+                                      {stat.stat.name}: 
+                                      </div> 
+                                      <div> {stat.base_stat} </div> 
+                                  </div>
+                              ))}
+                            
+                        </div>
+</div>
+
               </div>
 
             </div>
             <div className="flex justify-center pb-8">
               <button className="bg-red-600 w-36 h-10 text-slate-100 rounded-lg hover:bg-rose-600"
-              onClick={() => close()}>Close modal</button>
+              onClick={() => close()}>Close </button>
             </div>
           </div>
         )}
